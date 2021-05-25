32b9e0b4f94c905826fa9b2761482323d9232d20
Elian Braja <elian.braja@finetuning.it>
2021-05-25 02:35:32 +0200

    minor change

--- app/graphql/output_types/user_type.rb
+++ app/graphql/output_types/user_type.rb
@@ -5,6 +5,8 @@ module OutputTypes
 
     field :id, ID, 'user id', null: false
     field :email, String, 'user email', null: false
+    field :first_name, String, 'first name', null: false
+    field :last_name, String, 'last name', null: false
     field :token, String, 'user token', null:false
   end
 end
