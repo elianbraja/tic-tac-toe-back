15522c901fa32445ce5ebb9effb13e25958c7bfe
E Braja <elianbraja@gmail.com>
2021-06-09 13:51:19 +0200

    minor change

--- app/graphql/mutations/user/create_user.rb
+++ app/graphql/mutations/user/create_user.rb
@@ -1,7 +1,6 @@
 module Mutations::User
   class CreateUser < Mutations::BaseMutation
 
-
     argument :email, String, required: true
     argument :password, String, required:true
     argument :password_confirmation, String, required: true
